27c31e6e036c27d73a29028ebb0a8becc0b75872
dborra03 <davide.borra03@gmail.com>
2022-12-16 15:34:23 +0100

    site

---
+++ script.js
@@ -0,0 +1,33 @@
+var app = new Vue({
+    el: "#vueContainer",
+    data: {
+        width: 10,
+        bigScreen: true,
+        menuIsVisible: false,
+        menuItems: [
+            {
+                name: "Appunti",
+                pageSrc: "../appunti"
+            },
+            {
+                name: "LaTeX",
+                pageSrc: "../latex"
+            },
+            {
+                name: "Informatica",
+                pageSrc: "../informatica"
+            }
+        ],
+    },
+    mounted(){
+        this.width = window.innerWidth;
+        if (this.width < 800){
+            this.bigScreen=false;
+        }
+    },
+    methods: {
+        showMenu(){
+            this.menuIsVisible = !this.menuIsVisible && !this.bigScreen;
+        }
+    }
+});
