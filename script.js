0c6f8ca340b8d14054f858d0085cfdc83e843cfd
dborra03 <davide.borra03@gmail.com>
2023-01-04 15:33:53 +0100

    Added dark theme

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var app = new Vue({
     data: {
         width: 10,
         menuIsVisible: false,
+        dark: false,
         menuItems: [
             {
                 name: "Appunti",
@@ -23,6 +24,15 @@ var app = new Vue({
     methods: {
         showMenu(){
             this.menuIsVisible = !this.menuIsVisible;
+        },
+        changeTheme(){
+            var theme = document.getElementsByTagName('link')[1];
+            if (theme.getAttribute('href') == '../dark.css') {
+                theme.setAttribute('href', '');
+            } else {
+                theme.setAttribute('href', '../dark.css');
+            }
+            this.dark=!this.dark;
         }
     }
 });
